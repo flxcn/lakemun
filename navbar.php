9abf1bba28785f8832657a6c4e4690d0db5d6841
Felix <30987125+flxcn@users.noreply.github.com>
2021-01-26 14:51:07 -0600

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,10 +1,10 @@
 <div class="header">
-  <img href="index.php" src="logo.png" alt="logo" />
+  <img href="index.php" src="logo.png" alt="logo" >
   <a href="index.php" class="logo">Lake MUN</a>
   <div class="header-right">
-    <a  href="index.php">About Us</a>
-    <a class="active" href="com.php">Committees</a>
-    <a href="staff.php">Staff</a>
-    <a href="reg.php">Registration</a>
+    <a <?php if($pageName = 'index') echo "class='active'";?> href="index.php">About Us</a>
+    <a <?php if($pageName = 'com') echo "class='active'";?> href="com.php">Committees</a>
+    <a <?php if($pageName = 'staff') echo "class='active'";?> href="staff.php">Staff</a>
+    <a <?php if($pageName = 'reg') echo "class='active'";?> href="reg.php">Registration</a>
   </div>
 </div>
